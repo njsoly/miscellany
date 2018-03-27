de49239d777b91cf5761d67f8856e0b499308377
Nate Solyntjes <solyntjesn@my.uwstout.edu>
2018-03-27 18:06:25 -0500

    php-up-top.php: added comments
    The "(?)"s in there mean that I'm adding these to the repository and writing comments based on what I <i>think</i> I remember.
    Sue me.

--- php-up-top.php
+++ php-up-top.php
@@ -1,10 +1,14 @@
 <?php
+	/****************    This is a pile of document-starter PHP bits that I keep losing track of.    ***************/
+
+	/* This little bit will source your favorite header PHP */
 	$inc_file = '/php/standard-includes.php');
 	include($_SERVER['DOCUMENT_ROOT'] . $inc_file);
 ?>
 <?php 
-	display_errors("1");
-	ini_set("display_errors","On");
-	log_errors_max_len(0);
+	/* this lets the errors come through, so you can figure out what the hell has gone wrong.*/
+	display_errors("1");           		
+	ini_set("display_errors","On");		/* this is the inline equiv. of setting display_errors=1 in php.ini (?) */
+	log_errors_max_len(0);         		/* this takes off the limit on log error messages (?) */
 	
 ?>
