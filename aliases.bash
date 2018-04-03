413938faffafa5615ae8af38d34d1576e4040f79
Nate Solyntjes <njsoly@syntj.com>
2018-04-03 18:34:42 -0500

    +aliases.bash: favorite bash-shell aliases.

---
+++ aliases.bash
@@ -0,0 +1,12 @@
+# these are just some bash aliases that I like to have around.
+
+# WINDOWS shorthands
+alias npp='notepad++'
+
+# CROSS-PLATFORM conveniences
+alias path='echo $PATH $*'
+
+# GIT rapid-fire shit
+alias uncha='git update-index --assume-unchanged'
+alias remotev='git remote -v $*'
+alias remtv='git remote -v $*'
