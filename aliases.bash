251fb0b47d8273b61f43f1e9abe6f28cb2a1fe20
Nate Solyntjes <njsoly@syntj.com>
2018-04-03 19:24:19 -0500

    add -u ?

--- aliases.bash
+++ aliases.bash
@@ -1,12 +1,24 @@
 # these are just some bash aliases that I like to have around.
 
+
 # WINDOWS shorthands
 alias npp='notepad++'
 
 # CROSS-PLATFORM conveniences
 alias path='echo $PATH $*'
+alias fxn='function'
+alias lS='ls -A --size --block-size=KiB'
+alias lskb='ls -A -s --block-size=KiB'
+
 
 # GIT rapid-fire shit
+alias gs='git status $*'
+alias gsnu='git status -uno $*'
+alias gsuno='git status -uno $*'
 alias uncha='git update-index --assume-unchanged'
 alias remotev='git remote -v $*'
 alias remtv='git remote -v $*'
+alias fetcha='git fetch --all'
+alias ga='git add '
+alias gau='git add -u'
+alias pusha='git push --all $*'
--- make-cygdrive-links-on-root.bash
+++ make-cygdrive-links-on-root.bash
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-for char in {a..z}; 
-do [ -e /cygdrive/$char ] && [ ! -e /$char ] && ln -s /cygdrive/$char /$char && echo "made shortcut \"/$char\" to \"/cygdrive/$char\"."; 
+#############################################################################
+##  This is intended for use with Cygwin.
+## 
+##  - checks all the possible Windows drives (A: through Z:),
+##  checks whether they exist (are mounted in /cygdrive/ ),
+##  and makes a symbolic link to that same letter on root."
+#############################################################################
+
+for char in {a..z}; do 
+	[ -e /cygdrive/$char ] && [ ! -e /$char ] && \
+	ln -s /cygdrive/$char /$char && \
+	echo "made shortcut \"/$char\" to \"/cygdrive/$char\"." || \
+	echo "didn't make shortcut for /cygdrive/$char."
 done
