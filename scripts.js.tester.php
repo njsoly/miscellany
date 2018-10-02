982afa6a81a78cc2eaafb99bd1dcffc6fe9c0390
Nate Solyntjes <njsoly@syntj.com>
2018-10-02 00:32:17 -0500

    scripts.js.tester.php: added TITLE.

--- scripts.js.tester.php
+++ scripts.js.tester.php
@@ -20,6 +20,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
+	<title>:: scripts.js tester ::</title>
 	<script	src="https://code.jquery.com/jquery-2.2.4.min.js"	integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="	crossorigin="anonymous"></script>
 	<script src="scripts.js" type="text/javascript"></script>
 </head>
