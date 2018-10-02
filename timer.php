2406605a47e0538b60a1cdc8fcea80deb2c0b289
Nate Solyntjes <njsoly@syntj.com>
2018-10-02 03:12:18 -0500

    +timer.php: simple, pausable timer which counts seconds.

---
+++ timer.php
@@ -0,0 +1,114 @@
+<?php
+	include_once($_SERVER['DOCUMENT_ROOT'] . '/php/standard-includes.php');
+
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+
+	<title id=counterTitle>counter </title>
+	<script src="x.js" type="text/javascript"></script>
+	<meta charset="utf-8" />
+	<meta http-equiv='Pragma' content='no-cache' />
+	<link rel="stylesheet" type="text/css" href="styles.css" />
+	
+
+	<style>
+	.topDiv {
+		max-width: 99vw;
+		max-height: 99vh;
+		border: 1px dashed rgba(0,0,0,0.24);
+		background-color: rgba(0,200,255,0.5);
+	}
+	#timerDiv {
+		min-height: 10vh;
+		font-size: 2.5em;
+		text-align: center;
+		padding-top: 2.24vh;
+	}
+	button {
+		font-size: 3em;
+	}
+	fieldset {
+		background-color: rgba(0,48,20,0.4);
+	}
+html {
+background-color: #292933;
+color: rgb(222,224,240);
+}
+	</style>
+</head>
+<body>
+	<main>
+				
+		<div class="topDiv">
+			<div id="timerDiv" title="the timer.">
+			</div>
+			<div class=btns>
+				<fieldset>
+					<button onclick="toggleTimer();" id=timerToggle>
+					stop
+					</button>
+
+					<button onclick="resetTimer();">reset</button>
+				</fieldset>			
+			</div>
+
+		</div>
+	</main>
+	
+	<script id=timerDivJsScript>
+		timerCounter = 0;
+		timerRunning = false;
+		var x;
+		incrementTimer = function (){
+			document.getElementById("timerDiv").innerText = timerCounter + "s";
+			timerCounter++;
+		
+		};
+		resetTimer = function(){
+			timerCounter = -1;
+			/*if(timerRunning == true){
+				stopTimer();
+			}*/
+			incrementTimer();
+			incrementTimer();
+		};
+		function startTimer(){
+			(x = setInterval(
+				function(){
+					incrementTimer();
+				}, 1000
+			));
+			// this making too many timers
+			timerRunning = true;
+		};
+		
+		// this does not work.
+		function stopTimer(){
+			try{
+			clearInterval(x);
+			} catch(err){
+				console.error("didn't stop timer.");
+			}
+			timerRunning = false;
+		};
+		function toggleTimer(){
+			toggleButton = document.getElementById("timerToggle");
+			if(timerRunning == true){
+				stopTimer();
+				toggleButton.value = "start";
+			}
+			else {
+				startTimer();
+				toggleButton.value = "stop";
+			}
+			return timerRunning;
+		};
+		
+	</script>
+	<script>
+	startTimer();
+	</script>
+</body>
+</html>
