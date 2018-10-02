5d794daf0c7af22470370d841032278df2d2f453
Nate Solyntjes <njsoly@syntj.com>
2018-10-02 00:30:18 -0500

    scripts.js.tester.php: added several PHP INI settings.

--- scripts.js.tester.php
+++ scripts.js.tester.php
@@ -1,3 +1,22 @@
+<?php
+	ini_set("display_errors","0");
+
+	ini_set("log_errors_max_len", 0);
+	ini_set("display_startup_errors", (1));
+
+	error_reporting (E_ALL & ~E_STRICT & ~E_DEPRECATED);
+
+	ini_set("log_errors","On");
+	ini_set("error_log", ".error_log");
+
+	/* put last error msg. into $php_errormsg.*/
+	ini_set("track_errors", "on");	
+
+	/* ignore same errors from same line in */
+	ini_set("ignore_repeated_errors","off");
+	$x =8;
+	$x = $y;
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -5,7 +24,9 @@
 	<script src="scripts.js" type="text/javascript"></script>
 </head>
 <body title="" >
-	<div>	<h1>scripts.js tester</h1></div>
+	<div>	<h1>scripts.js tester</h1>
+		<a href=".error_log">errors log</a>
+	</div>
 	<div id="isJQueryLoaded" >
 		
 	</div>
