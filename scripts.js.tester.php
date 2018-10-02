bb00b098474f965cfc8908cb6314f01a1a4bbccd
Nate Solyntjes <njsoly@syntj.com>
2018-10-02 00:14:21 -0500

    +scripts.js.tester.php: a tester PHP for scripts.js

---
+++ scripts.js.tester.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<script	src="https://code.jquery.com/jquery-2.2.4.min.js"	integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="	crossorigin="anonymous"></script>
+	<script src="scripts.js" type="text/javascript"></script>
+</head>
+<body title="" >
+	<div>	<h1>scripts.js tester</h1></div>
+	<div id="isJQueryLoaded" >
+		
+	</div>
+	
+</body>
+</html>
