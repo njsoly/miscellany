38251a0f89b2ea1476ad203f6e98ede1059cf40d
Nate Solyntjes <njsoly@syntj.com>
2018-10-03 02:08:13 -0500

    aliases.bash: added some things, tidied others.
    - changed around some groupings in order to make more sense
    - put aliases under fancy new comment headers
    - removed unnecessary inclusion of all parameters ("")

--- aliases.bash
+++ aliases.bash
@@ -3,22 +3,30 @@
 
 # WINDOWS shorthands
 alias npp='notepad++'
+alias notepadpp='notepad++'
 
-# CROSS-PLATFORM conveniences
+# MIMICRY OF CMD.EXE THINGS :: COMPATIBLE CROSS-PLATFORM #
 alias path='echo $PATH $*'
-alias fxn='function'
+
+
+####    MY BASH CUSTOMIZATIONS   ####
 alias lS='ls -A --size --block-size=KiB'
 alias lskb='ls -A -s --block-size=KiB'
-
+alias fxn='function'
 
 # GIT rapid-fire shit
 alias gs='git status $*'
 alias gsnu='git status -uno $*'
 alias gsuno='git status -uno $*'
-alias uncha='git update-index --assume-unchanged'
-alias remotev='git remote -v $*'
-alias remtv='git remote -v $*'
+alias guncha='git update-index --assume-unchanged'
+alias remotev='git remote -v'
+alias remtv='git remote -v'
 alias fetcha='git fetch --all'
 alias ga='git add '
 alias gau='git add -u'
 alias pusha='git push --all $*'
+
+####  A FEW ACCIDENTAL-CAPS MISTAKES TO OVERLOOK  ####
+alias ALIAS='alias'
+alias EXIT='exit'
+
