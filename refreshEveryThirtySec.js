f3629ae83b0440c88f44b8ea1589b508bdeeb676
njsoly <njsoly@syntj.com>
2018-10-04 10:13:16 -0500

    +refreshEveryThirtySec.js

---
+++ refreshEveryThirtySec.js
@@ -0,0 +1,34 @@
+/* refreshEveryThirtySec.js
+ *
+ * This is code I wrote to automatically find the "Refresh" button in SalesForce, every 30 seconds.
+ */
+
+refreshButton = document.getElementsByClassName("btn refreshListButton")[0];
+
+/* this appears to only work after using Developer Tools to select the refresh button. */
+function refreshEveryThirtySec(f){
+	if(typeof f == "undefined"){
+
+		function fxn(){
+			refreshButton = document.getElementsByClassName("btn refreshListButton")[0];
+			if(typeof refreshButton == "undefined"){
+				console.error("something went wrong finding the refresh button.");
+				return;
+			}
+			console.info("clicking the refresh button.");
+			
+refreshButton.click();			
+		};
+	}
+	else {
+		fxn = f;
+	}
+	interval = 30000;
+	
+	var x = setInterval(fxn, interval);
+	console.debug("set interval to refresh list every " + interval + " ms.");
+	
+	if(typeof alerts == "undefined" || alerts == true){ alert("set interval to refresh list every " + interval + " ms.");	}
+};
+
+refreshEveryThirtySec();
