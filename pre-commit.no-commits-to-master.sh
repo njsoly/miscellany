3366693eba8d35f4c1f905d8f81a5863a1f46bbe
Nate Solyntjes <njsoly@syntj.com>
2019-01-31 02:57:34 -0600

    pre-commit.no-commits-to-master.sh: new file to help against some ofthe stupid mistakes.

--- pre-commit.no-commits-to-master.sh
+++ pre-commit.no-commits-to-master.sh
@@ -2,13 +2,13 @@
 
 # I found this online at StackOverflow.  
 # Should help with preventing some rookie mistakes.
-# 
+#
 # They say you don't need to set it executable if you're on Windows,
 # but we'll see.
 #
 # This file is to be put inside a repository's .git/ folder,
 # as ${REPOSITORY_ROOT}/.git/hooks/pre-commit .
-# 
+#
 
 branch="$(git rev-parse --abbrev-ref HEAD)"
 
