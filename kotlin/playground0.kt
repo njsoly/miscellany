7acf3fb56283f85f9319dfccceaa17fd2bd5afe9
Nate Solyntjes <njsoly@syntj.com>
2019-05-05 23:59:09 -0500

    added link

--- kotlin/playground0.kt
+++ kotlin/playground0.kt
@@ -1,5 +1,8 @@
-val l: List<Int> = (1..9).toList()
+/** 
+ * https://play.kotlinlang.org/#eyJ2ZXJzaW9uIjoiMS4zLjMxIiwicGxhdGZvcm0iOiJqYXZhIiwiYXJncyI6IiIsImpzQ29kZSI6IiIsIm5vbmVNYXJrZXJzIjp0cnVlLCJ0aGVtZSI6ImlkZWEiLCJjb2RlIjoidmFsIGw6IExpc3Q8SW50PiA9ICgxLi45KS50b0xpc3QoKVxuXG5cblxuXG5cblxuZnVuIG1haW4oKSB7XG4gICAgcHJpbnRsbihcIkhlbGxvLCB3b3JsZCEhISBcIiArIGwpXG4gICAgZm9yKGkgaW4gbCl7XG4gICAgICAgIHByaW50bG4oXCJpOiAkaTsgbDogJHtsLnRvTGlzdCgpLnRvU3RyaW5nKCl9XCIpXG4gICAgfVxuICAgIFxuICAgIHZhbCBvZGRzID0gbC5maWx0ZXJ7eCAtPiB4ICUgMiAhPSAwfVxuICAgIHZhbCBldmVucyA9IGwuZmlsdGVye2l0ICUgMiA9PSAwfVxuICAgIHByaW50bG4oXCJvZGRzOiAkb2RkcywgZXZlbnM6ICRldmVuc1wiKVxufVxuXG5cbiJ9
+ */
 
+val l: List<Int> = (1..9).toList()
 
 fun main() {
     println("Hello, world!!! " + l)
