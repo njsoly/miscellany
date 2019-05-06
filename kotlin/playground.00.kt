e6fcbd391840c0fab80b53e380fe7c95f62897ca
Nate Solyntjes <njsoly@syntj.com>
2019-05-06 01:53:32 -0500

    Update playground.00.kt

--- kotlin/playground.00.kt
+++ kotlin/playground.00.kt
@@ -1,23 +1,19 @@
 /** 
  * https://play.kotlinlang.org/#eyJ2ZXJzaW9uIjoiMS4zLjMxIiwicGxhdGZvcm0iOiJqYXZhIiwiYXJncyI6IiIsImpzQ29kZSI6IiIsIm5vbmVNYXJrZXJzIjp0cnVlLCJ0aGVtZSI6ImlkZWEiLCJjb2RlIjoidmFsIGw6IExpc3Q8SW50PiA9ICgxLi45KS50b0xpc3QoKVxuXG5cblxuXG5cblxuZnVuIG1haW4oKSB7XG4gICAgcHJpbnRsbihcIkhlbGxvLCB3b3JsZCEhISBcIiArIGwpXG4gICAgZm9yKGkgaW4gbCl7XG4gICAgICAgIHByaW50bG4oXCJpOiAkaTsgbDogJHtsLnRvTGlzdCgpLnRvU3RyaW5nKCl9XCIpXG4gICAgfVxuICAgIFxuICAgIHZhbCBvZGRzID0gbC5maWx0ZXJ7eCAtPiB4ICUgMiAhPSAwfVxuICAgIHZhbCBldmVucyA9IGwuZmlsdGVye2l0ICUgMiA9PSAwfVxuICAgIHByaW50bG4oXCJvZGRzOiAkb2RkcywgZXZlbnM6ICRldmVuc1wiKVxufVxuXG5cbiJ9
- * https://pl.kotl.in/oTCV_bZrE
- * 2019-05-06
  */
 
-val l: List<Int> = (1..9).toList()
 
-// some filter fun
-val odds = l.filter{x -> x % 2 != 0}
-val evens = l.filter{it % 2 == 0}
 
 var b: String = "snth"
 // the long way to do a "use if not null", without elvis operator
 val c: Int = if (b != null) b.length else -1
-// shorter way to do it, WITH elvis operator.
+// a shorter way to do it, WITH elvis operator.
 val d = b?.length ?: -1
 
+val aInt: Int? = c as? Int
 
-fun dealWithX(x: Any){
+
+fun dealWithAnyX(x: Any){
     // example of WHEN-IS block
     when (x) {
     is Int -> print(x + 1)
@@ -26,11 +22,23 @@ fun dealWithX(x: Any){
 	}
 }
 
-fun main() {
-    println("Hello, world!!! " + l)
+fun funWithFilters (){
+    val l: List<Int> = (1..9).toList()
+
+    // some filter fun
+    val odds = l.filter{x -> x % 2 != 0}
+    val evens = l.filter{it % 2 == 0}
+
     for(i in l){
         println("i: $i; l: ${l.toList().toString()}")
     }
     
     println("odds: $odds, evens: $evens")
+}
+fun main() {
+    println("Hello, world!!! " + l)
+    
+    funWithFilters()
+    dealWithAnyX()
+
 }
