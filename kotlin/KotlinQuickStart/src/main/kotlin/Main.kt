477d12871333081b2e5b2f794a14c5188a58ed8c
njsoly <njsoly@syntj.com>
2019-07-14 11:05:16 -0500

    kotlin/Main.kt: new fxn rev().

--- kotlin/KotlinQuickStart/src/main/kotlin/Main.kt
+++ kotlin/KotlinQuickStart/src/main/kotlin/Main.kt
@@ -1,8 +1,17 @@
 fun main(args: Array<String>) {
 	printHello()
 	printHowManyArgs(args)
+	val s = "snth"
+	println("reverse of $s is " + rev(s))
 }
 
+fun rev(s : String) : String {
+	var r = s.toCharArray()
+	for(i in 0..(s.length - 1)){
+		r[i] = s.get(s.length - 1 - i)
+	}
+	return String(r)
+}
 fun printHowManyArgs(args: Array<String>){
 	println("there are ${args.size} args.")
 }
