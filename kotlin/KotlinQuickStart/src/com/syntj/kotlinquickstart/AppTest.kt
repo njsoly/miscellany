2fad9d8715fdf63281850e1040b310a10127d182
njsoly <njsoly@syntj.com>
2019-07-17 20:00:04 -0500

    KotlinQuickStart/: a lot

--- kotlin/KotlinQuickStart/src/com/syntj/kotlinquickstart/AppTest.kt
+++ kotlin/KotlinQuickStart/src/com/syntj/kotlinquickstart/AppTest.kt
@@ -1,10 +1,16 @@
 package com.syntj.kotlinquickstart
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class AppTest {
 
 	var x: Int = 0
+
+    @Mock
+    var f: Fish
 
 	@Test
 	fun yourTest() {
