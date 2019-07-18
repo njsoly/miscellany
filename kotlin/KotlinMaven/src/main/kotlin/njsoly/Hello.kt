e2df54dc74e46d6c66049d225bb1e8c94c539634
njsoly <njsoly@syntj.com>
2019-07-17 19:58:59 -0500

    kotlin: new project KotlinMaven.

---
+++ kotlin/KotlinMaven/src/main/kotlin/njsoly/Hello.kt
@@ -0,0 +1,7 @@
+package njsoly
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+     
+}
+
---
+++ kotlin/KotlinMaven/src/test/kotlin/njsoly/HelloTest.kt
@@ -0,0 +1,8 @@
+package njsoly
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
