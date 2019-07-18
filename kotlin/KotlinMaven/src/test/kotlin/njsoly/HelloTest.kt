210f11ec13b116f8493cbbf8f0f9fa0c98b4ee95
njsoly <njsoly@syntj.com>
2019-07-17 20:59:31 -0500

    kotlin: KotlinMaven project now works with MOCKITO!

--- kotlin/KotlinMaven/src/test/kotlin/njsoly/HelloTest.kt
+++ kotlin/KotlinMaven/src/test/kotlin/njsoly/HelloTest.kt
@@ -1,8 +1,31 @@
 package njsoly
 
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.MockitoAnnotations
+
+import org.mockito.runners.MockitoJUnitRunner
 import kotlin.test.assertEquals
 
+/**
+ * I had to do some weird shit with the Maven tab in IntelliJ to make this work correctly.
+ */
+@RunWith(MockitoJUnitRunner::class)
 class HelloTest {
 
+    @MockitoAnnotations.Mock
+    lateinit var a: MutableList<Int>
+
+    @Before
+    fun setup () {
+        a = ArrayList<Int>()
+        a.add(-1)
+
+    }
+
+    @Test
+    fun `what do you want` () {
+        assertEquals(a[0], (-1))
+    }
 }
