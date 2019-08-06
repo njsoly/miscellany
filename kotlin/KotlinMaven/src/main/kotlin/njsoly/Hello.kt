df99618fc5b9043c3609a523fdb9d7245ee059aa
njsoly <njsoly@syntj.com>
2019-08-05 22:45:04 -0500

    Kotlin: Hello.kt - added simple class and exemplified destructured parameters

--- kotlin/KotlinMaven/src/main/kotlin/njsoly/Hello.kt
+++ kotlin/KotlinMaven/src/main/kotlin/njsoly/Hello.kt
@@ -1,7 +1,17 @@
 package njsoly
 
+
+class SevenZ  (val n: Int = 7, val s: String = "Z") {
+    operator fun component1(): Int = n
+    operator fun component2(): String = s
+}
+
 fun main(args: Array<String>) {
     println("Hello, World")
-     
+
+    val sevenZ = SevenZ()
+    val (number, letter) = sevenZ
+
+    println("Destructured number: $number, destructured letter: $letter")
 }
 
