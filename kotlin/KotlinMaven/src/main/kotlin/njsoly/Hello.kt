5187e93bdffd2aa70dd7199c8a2619ed1a8f6eb6
njsoly <njsoly@syntj.com>
2019-08-05 22:59:14 -0500

    Kotlin files: moved SevenZ to own file

--- kotlin/KotlinMaven/src/main/kotlin/njsoly/Hello.kt
+++ kotlin/KotlinMaven/src/main/kotlin/njsoly/Hello.kt
@@ -1,11 +1,6 @@
 package njsoly
 
 
-class SevenZ  (val n: Int = 7, val s: String = "Z") {
-    operator fun component1(): Int = n
-    operator fun component2(): String = s
-}
-
 fun main(args: Array<String>) {
     println("Hello, World")
 
