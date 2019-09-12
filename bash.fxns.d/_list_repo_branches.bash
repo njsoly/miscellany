63f1acb0065155859c99c0480613978feb17ed3c
njsoly <njsoly@syntj.com>
2019-09-12 04:50:27 -0500

    _list_repo_branches.bash: handle nonexistent genesis-projects

--- bash.fxns.d/_list_repo_branches.bash
+++ bash.fxns.d/_list_repo_branches.bash
@@ -1,19 +1,21 @@
 _list_repo_branches () 
 { 
     local branches_file=~/.branches
-    local genesis_projects_directory=~/genesis-projects
+    local projects_directory=~/genesis-projects
+    
+    if [[ ! -d $projects_directory ]]; then
+        projects_directory=$PWD
+    fi
 
-    pushd $genesis_projects_directory > /dev/null;
+    pushd $projects_directory > /dev/null;
     [ -f $branches_file ] && rm $branches_file;
-    for f in $genesis_projects_directory ./*;
-    do
-        { 
-            [ -d "$f" ] && [ -d "$f/.git/" ] && { 
-                pushd $f > /dev/null;
-                printf "%s: %s\n" "${f//*\//}" "$(git branch | grep \*)" >> $branches_file;
-                popd > /dev/null
-            } > /dev/null
-        };
+    for f in $projects_directory ./*;
+    do 
+        [ -d "$f" ] && [ -d "$f/.git/" ] && { 
+            pushd $f > /dev/null;
+            printf "%s: %s\n" "${f//*\//}" "$(git branch | grep \*)" >> $branches_file;
+            popd > /dev/null
+        }
     done;
     popd > /dev/null
     cat $branches_file
