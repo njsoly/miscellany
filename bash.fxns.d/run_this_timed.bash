b8d943322dd7f04fe3a9a660fd3b41a305993baf
njsoly <njsoly@syntj.com>
2019-10-09 16:12:32 -0500

    run_this_timed.bash: readable elapsed time

--- bash.fxns.d/run_this_timed.bash
+++ bash.fxns.d/run_this_timed.bash
@@ -2,24 +2,40 @@
 
 # note: this doesn't work with the standard MacOS bash.
 # you need bash-extras or something.
+millis_to_readable () {
+	DEBUG=0
+	x=$1
+	[[ -n "$x" ]] || ( echo "must pass a parameter"; exit 1 )
+	m=$[$x / 60 / 1000]
+	s=$[x / 1000 - ($m * 60)]
+	ms=$[$x % 1000]
+#	echo "$x millis: ${m}m, ${s}.${ms}s"
+	[[ ! $m == 0 ]] && (
+		echo "${m}m, ${s}.${ms}s"
+	) || (
+		echo "${s}.${ms}s"
+	)
+}
 
 run_this_timed () {
-	local start_stop_logfile="~/.time_ab.log"
-	local time_elapsed_logfile="~/.time_elapsed.log"
-	local debug_logfile="~/.run_this_timed_debug"
+	local start_stop_logfile="$HOME/.time_ab.log"
+	local time_elapsed_logfile="$HOME/.time_elapsed.log"
+	local debug_logfile="$HOME/.run_this_timed_debug"
+	local DEBUG=0
     _command="$@";
 
     _start=$(date +%H:%M:%S.%N);
     _start=${_start:0:12};
     _start_seconds=$(date +%s);
-    echo "_start_seconds: $_start_seconds" >> ${debug_logfile}
     _start_date=$(date +%F);
     _start_milliseconds=${_start_seconds}${_start: -3}
-    echo "_start_milliseconds: $_start_milliseconds" >> ${debug_logfile}
-    _msg="starting \"${_command}\" (in $PWD) at $_start
-$_start_date";
+    _msg="starting \"${_command}\" (in $PWD) at $_start $_start_date";
+
+    [[ ! "$DEBUG" = 0 ]] && echo "_start_seconds: $_start_seconds" >> ${debug_logfile}
+    [[ ! "$DEBUG" = 0 ]] && echo "_start_milliseconds: $_start_milliseconds" >> ${debug_logfile}
+
     echo ${_msg};
-    echo ${_msg} >> ~/.time_ab.log
+    echo ${_msg} >> ${start_stop_logfile}
 
     ${_command}
     _exit_status=$?
@@ -29,14 +45,14 @@ $_start_date";
     _finish_seconds=$(date +%s);
     _finish_date=$(date +%F);
     _finish_milliseconds=${_finish_seconds}${_finish: -3}
-    _msg="finished \"${_command}\" (in $PWD) at ${_finish}
-${_finish_date} [exit code: ${_exit_status}]";
-    echo ${_msg};
-    echo ${_msg} >> ~/.time_ab.log;
-
-    _elapsed=$[ ${_finish_milliseconds} - $_start_milliseconds ]
-    _msg="command \"${_command}\" [in $PWD] ran from $_start $_start_date to ${_finish} ($_elapsed ms) [exit code: ${_exit_status}]"
+    _msg="finished \"${_command}\" (in $PWD) at ${_finish} ${_finish_date} [exit code: ${_exit_status}]";
     echo ${_msg};
     echo ${_msg} >> ${start_stop_logfile};
-    return ${_exit_status}
+
+    _elapsed=$[ ${_finish_milliseconds} - $_start_milliseconds ]
+    _elapsed_readable=$(millis_to_readable $_elapsed)
+    _msg="command \"${_command}\" [in $PWD] ran from $_start $_start_date to ${_finish} ($_elapsed_readable) [exit code: ${_exit_status}]"
+    echo ${_msg};
+    echo ${_msg} >> ${start_stop_logfile};
+    return ${_exit_status};
 }
