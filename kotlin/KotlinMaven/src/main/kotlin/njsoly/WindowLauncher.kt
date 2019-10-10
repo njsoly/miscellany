02c272a6949d6935f0fbe13c3b7d5026a7866f16
njsoly <njsoly@syntj.com>
2019-10-10 02:11:21 -0500

    WindowLauncher and ~Test: progress, doesn't lay out right.

--- kotlin/KotlinMaven/src/main/kotlin/njsoly/WindowLauncher.kt
+++ kotlin/KotlinMaven/src/main/kotlin/njsoly/WindowLauncher.kt
@@ -1,36 +1,58 @@
 package njsoly
 
-import java.awt.BorderLayout
-import java.awt.FlowLayout
+import java.awt.*
 import javax.swing.*
 
+class Cv : Canvas() {
+
+    override fun paint(g: Graphics?) {
+        super.paint(g)
+        val g2: Graphics2D = g as Graphics2D
+        g2.color = Color.BLUE
+        g2.fillRect(10,10,100,100)
+        g2.background = Color.BLACK
+    }
+}
 
 class WindowLauncher() : JFrame() {
     constructor(title: String) : this() {
         this.title = title
 
+        canvas.preferredSize = Dimension(400, 400)
+        canvasPanel.add(canvas)
     }
 
     var flowLayout: FlowLayout = FlowLayout(FlowLayout.CENTER, 5, 5)
 
     var borderLayout = BorderLayout(5, 5)
 
+    val canvasPanel = JPanel(true)
+    private val canvas = Cv()
+
     private var stuff: Map<JComponent, String> = mapOf<JComponent, String> (
         JLabel("I am a window launcher") to BorderLayout.NORTH,
-        JButton("click this and live") to BorderLayout.CENTER
+        JButton("click this and live") to BorderLayout.SOUTH,
+        canvasPanel to BorderLayout.CENTER
     )
 
     init {
+//        layout = borderLayout
         layout = flowLayout
         this.defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-        SwingUtilities.invokeLater { this.addStuff() }
-        SwingUtilities.invokeLater { this.pack() }
+        SwingUtilities.invokeLater { setComponentsToPreferred() }
+        SwingUtilities.invokeLater {
+            canvas.setSize(1280, 720)
+            canvasPanel.doLayout()
+        }
+        SwingUtilities.invokeLater { addStuff() }
+        SwingUtilities.invokeLater { pack() }
     }
 
     fun addStuff(){
         SwingUtilities.invokeLater {
             when (layout) {
                 is BorderLayout -> {
+                    this.contentPane.layout = this.borderLayout
                     stuff.forEach { this.contentPane.add(it.key, it.value) }
                 }
                 is FlowLayout -> {
@@ -41,21 +63,31 @@ class WindowLauncher() : JFrame() {
                 }
             }
         }
-        SwingUtilities.invokeLater {
-            this.components.forEach {
-                it.size = it.preferredSize
-            }
+    }
+
+    fun setComponentsToPreferred() {
+        this.components.forEach {
+            it.size = it.preferredSize
         }
-        SwingUtilities.invokeLater { this.pack() }
     }
 }
 
 fun main(args: Array<String>) {
     val w = WindowLauncher("WindowLauncher")
-//    w.addStuff()
 
     SwingUtilities.invokeLater {
         w.isVisible = true
+
+        w.components.plus(w.contentPane.components)
+            .plus(w.rootPane.components)
+            .plus(w.contentPane)
+            .forEachIndexed { i, component ->
+            println("component $i: ${component.name} (${component.javaClass.simpleName})")
+        }
+    }
+
+    SwingUtilities.invokeLater {
+        w.pack()
         w.setLocationRelativeTo(null)
     }
 
---
+++ kotlin/KotlinMaven/src/test/kotlin/njsoly/WindowLauncherTest.kt
@@ -0,0 +1,48 @@
+package njsoly
+
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class WindowLauncherTest {
+
+    lateinit var windowLauncher: WindowLauncher
+
+    @Before
+    fun setUp() {
+        windowLauncher = WindowLauncher("test WindowLauncher")
+    }
+
+    @Test
+    fun getFlowLayout() {
+    }
+
+    @Test
+    fun setFlowLayout() {
+    }
+
+    @Test
+    fun getBorderLayout() {
+    }
+
+    @Test
+    fun setBorderLayout() {
+    }
+
+    @Test
+    fun getCanvasPanel() {
+    }
+
+    @Test
+    fun addStuff() {
+    }
+
+    @Test
+    fun setComponentsToPreferred() {
+    }
+
+    @Test
+    fun paintCanvas() {
+    }
+}
