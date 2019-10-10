3a07404ff69935cbc7047fd8ed92fb8d4b1166c8
njsoly <njsoly@syntj.com>
2019-10-10 11:56:20 -0500

    millis_to_readable.bash: updated with changes from run_this_timed

--- millis_to_readable.bash
+++ millis_to_readable.bash
@@ -1,1 +1,15 @@
-m=$[$x / 60 / 1000]; s=$[x / 1000 - ($m * 60)]; ms=$[$x % 1000]; echo "$x millis: ${m}m, ${s}.${ms}s"
+#!/usr/bin/env bash
+[[ $(type "millis_to_readable" 2> /dev/null) ]] || millis_to_readable () {
+	DEBUG=0
+	x=$1
+	[[ -n "$x" ]] || ( echo "must pass a parameter"; exit 1 )
+	m=$[$x / 60 / 1000]
+	s=$[x / 1000 - ($m * 60)]
+	ms=$[$x % 1000]
+#	echo "$x millis: ${m}m, ${s}.${ms}s"
+	[[ ! $m == 0 ]] && (
+		echo "${m}m ${s}.${ms}s"
+	) || (
+		echo "${s}.${ms}s"
+	)
+}
