e09b141f59efea58c4948f22e92aa462e37ad458
njsoly <njsoly@syntj.com>
2019-11-02 00:18:15 -0500

    Hello.kt: get out

--- kotlin/KotlinMaven/src/main/kotlin/njsoly/Hello.kt
+++ kotlin/KotlinMaven/src/main/kotlin/njsoly/Hello.kt
@@ -1,16 +1,11 @@
 package njsoly
 
-import java.io.File
 import java.time.LocalDate
 
 
 fun main(args: Array<String>) {
     println("Hello, World")
 
-    val (number, letter) = SevenZ()
-
-    val file: File = File("twl.txt")
-    println("File ${file.name} exists: ${file.exists()}")
     val sevenZ = SevenZ()
     val (number, letter) = sevenZ
 
