c2c589e97bf9c6f6cc6a850482f91e40f0be0a71
njsoly <njsoly@syntj.com>
2019-11-03 10:44:19 -0600

    batch/: startup files.  changed one into two, and made 'em work together.

--- batch/.batchStartup.bat
+++ batch/.batchStartup.bat
@@ -4,9 +4,6 @@
 :: I leave it up to me (or whomever uses this) to put a call for it in 
 :: the Programs/Startup folder in order to use it.
 ::
-:: ... actually maybe it would be a cool thing to add a section in
-:: here that writes a simple .BAT file to that directory, with only
-:: one line to call userStartup.bat.
 ::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::
@@ -28,25 +25,15 @@
 :SETUP
 	@echo off
 	@echo starting "%0"
+	echo miscellany is %miscellany%
 	
-:DECIDE
+:CHECK_PATH
 	where gsnu 2> NUL
-	@if NOT ERRORLEVEL 1 goto ALREADY_THERE
+	@if ERRORLEVEL 1 (call %miscellany%\batch\.userStartup)
 	
-:SHRINK_THE_PATH
-call shrink_path
-
-:APPEND_BATCH_DIR_TO_PATH
-	@echo adding "%CD%" to the path.
-	set PATH=%PATH%;%CD%
-	setx PATH "%PATH%;%CD%"
-	@if ERRORLEVEL 1 (echo PATH changes didn't work.)
-	@goto BYE
-
-:ALREADY_THERE
-@echo %CD% is already in path.
+:PUSHD_MISC
+	@echo on
+	pushd %miscellany%
 
 :BYE
 	@echo finished "%0"
-	@exit /B
-
