1a14a85806e308182976f844679578babb9202a6
njsoly <njsoly@syntj.com>
2019-11-12 02:32:38 -0600

    WordSearcher: LetterTile enum finished

--- kotlin/KotlinMaven/wordSearcher/src/main/kotlin/njsoly/wordsearcher/LetterTile.kt
+++ kotlin/KotlinMaven/wordSearcher/src/main/kotlin/njsoly/wordsearcher/LetterTile.kt
@@ -1,10 +1,32 @@
 package njsoly.wordsearcher
 
 @Suppress("KDocMissingDocumentation")
-enum class LetterTile (val points: Int){
-    A(1),
-    B(3),
+enum class LetterTile (val points: Int=1){
+    A,
+    B(4),
     C(4),
+    D(2),
+    E,
+    F(4),
+    G(3),
+    H(3),
+    I,
+    J(10),
+    K(5),
+    L(2),
+    M(4),
+    N(2),
+    O,
+    P(4),
+    Q(10),
+    R,
+    S,
+    T,
+    U(2),
+    V(5),
+    W(4),
+    X(8),
+    Y(3),
     Z(10)
 
 }
