644ec1e90a253088442605e27d7fc1dd7d92ae74
njsoly <njsoly@syntj.com>
2019-11-16 09:44:45 -0600

    What.java now has working logger (just to standard output)

--- kotlin/KotlinMaven/src/main/java/njsoly/What.java
+++ kotlin/KotlinMaven/src/main/java/njsoly/What.java
@@ -1,9 +1,16 @@
 package njsoly;
 
+import com.polymuler.njs.jdk6.log.Logginit;
+import com.polymuler.njs.jdk6.log.SetupLogger;
+import org.apache.log4j.Logger;
+
 public class What {
+
+    private static Logger l = SetupLogger.setupLogger(What.class);
 
     public static void main(String[] args){
         System.out.println("SNTH");
+        l.info("SNTHOEUSNTOHESUNTH");
     }
 
 }
