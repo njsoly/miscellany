51cd184ff3ac58d24ad0a5d23fb4ed7d26fafdb6
njsoly <njsoly@syntj.com>
2019-11-17 07:02:59 -0600

    kotlin: .idea/libraries

---
+++ kotlin/KotlinMaven/loggerFactoryUsageTry/src/test/kotlin/njsoly/log/LoggerFactoryUsageTest.kt
@@ -0,0 +1,15 @@
+package njsoly.log
+
+import org.junit.Test
+
+class LoggerFactoryUsageTest : LoggerFactoryUsage() {
+
+    @Test
+    fun testGetL() {
+    }
+
+    fun runMain() {
+        main(listOf("aoeu","snth"))
+
+    }
+}
