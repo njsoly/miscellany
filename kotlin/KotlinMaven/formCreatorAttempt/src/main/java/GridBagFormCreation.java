ff360030cff6896a40249a0a2ae6f6034ef72369
njsoly <njsoly@syntj.com>
2019-12-26 17:21:19 -0600

    KotlinMaven/: moved color constants from GridBagFormCreation to new class

--- kotlin/KotlinMaven/formCreatorAttempt/src/main/java/GridBagFormCreation.java
+++ kotlin/KotlinMaven/formCreatorAttempt/src/main/java/GridBagFormCreation.java
@@ -1,4 +1,5 @@
 import kotlin.Pair;
+import njsoly.util.ColorUtil;
 
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
@@ -18,6 +19,8 @@ import static java.awt.Color.*;
 import static java.awt.Font.*;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
+import static njsoly.util.ColorUtil.*;
+
 @SuppressWarnings("unused")
 public class GridBagFormCreation {
 	public JPanel mainPanel;
@@ -29,21 +32,6 @@ public class GridBagFormCreation {
 	private JEditorPane htmlPane;
 	protected HTMLDocument htmlDocument;
 
-	public static final Color BRIGHT_SEAFOAM = new Color(78, 255, 190);
-	public static final Color DEEP_GREEN_BLUE = new Color(0, 43, 29);
-	public static final Color DEEP_BLUE = new Color(0, 31, 43);
-	public static final Color LOSER_BLUE = new Color(180, 228, 245);
-	public static final Color BETTER_MAGENTA_PINK = new Color(255, 96, 155);
-	public static final Color BETTER_MAGENTA_MED_LIGHT = new Color(253, 159, 255);
-	public static final Color BETTER_PURPLE = new Color(164, 93, 249);
-	public static final Color MED_GRAY_BLUE_GREEN = new Color(50, 86, 99);
-	public static final Color BETTER_TEAL = new Color(0, 212, 201);
-	public static final Color SKY_BLUE_1 = new Color(116, 192, 237);
-	public static final Color SKY_BLUE_2 = new Color(69, 170, 237);
-	public static final Color SKY_BLUE_3 = new Color(0, 160, 230);
-	public static final Color SOFTER_CYAN = new Color(132, 249, 255);
-	public static final Color SOFTER_SEAFOAM = new Color(192, 255, 230);
-	public static final Color SLATE = new Color(25, 26, 34);
 
 	protected static final HashMap<String, Pair<LocalDateTime, LocalDateTime>> TIMINGS = new HashMap<>();
 
@@ -92,7 +80,7 @@ public class GridBagFormCreation {
 
 		this.addStandardKeyListeners();
 
-		mainPanel.setBorder(BorderFactory.createLineBorder(DEEP_GREEN_BLUE, 2, true));
+		mainPanel.setBorder(BorderFactory.createLineBorder(ColorUtil.DEEP_GREEN_BLUE, 2, true));
 
 		resultsPane.setBackground(DEEP_GREEN_BLUE); // med cobalt blue
 		resultsPane.setForeground(SOFTER_CYAN); // bright-ish cyan
@@ -116,7 +104,7 @@ public class GridBagFormCreation {
 
 		setBgFg(resultsTextArea, BLACK, SOFTER_SEAFOAM);
 		setBgFg(searchTextArea, DEEP_BLUE, WHITE);
-		setBgFg(htmlPane, DEEP_GREEN_BLUE, SOFTER_CYAN);
+		setBgFg(htmlPane, ColorUtil.DEEP_GREEN_BLUE, SOFTER_CYAN);
 
 		htmlPane.setDisabledTextColor(GRAY);
 		htmlPane.setSelectedTextColor(WHITE);
