2fc164161f1628e9279b198e4f3aa57286d82b0f
njsoly <njsoly@syntj.com>
2019-12-26 21:48:12 -0600

    WordSearcher GUI: follow renames in GridBagFormCreation

--- kotlin/KotlinMaven/wordSearcher/src/main/kotlin/njsoly/wordsearcher/WordSearcherGuiDriver.kt
+++ kotlin/KotlinMaven/wordSearcher/src/main/kotlin/njsoly/wordsearcher/WordSearcherGuiDriver.kt
@@ -1,15 +1,12 @@
 package njsoly.wordsearcher
 
-import java.awt.event.KeyAdapter
-import java.awt.event.KeyEvent
-
 class WordSearcherGuiDriver {
 
     val window: WordSearcherWindow = WordSearcherWindow()
     val form: WordSearcherForm = window.wordSearcherForm
 
     init {
-        form.inputArea
+        form.inputTextArea
     }
 
 }
