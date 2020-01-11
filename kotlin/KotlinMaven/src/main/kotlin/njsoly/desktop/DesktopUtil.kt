122e16d1b44e08e713ecabaab55aaef069f30e92
njsoly <njsoly@syntj.com>
2020-01-10 18:53:03 -0600

    DesktopUtil start

--- kotlin/KotlinMaven/src/main/kotlin/njsoly/desktop/DesktopUtil.kt
+++ kotlin/KotlinMaven/src/main/kotlin/njsoly/desktop/DesktopUtil.kt
@@ -1,1 +1,15 @@
 package njsoly.desktop
+
+import java.awt.GraphicsDevice
+import java.awt.GraphicsEnvironment
+
+class DesktopUtil {
+
+    val gd: GraphicsDevice = GraphicsEnvironment.getLocalGraphicsEnvironment().defaultScreenDevice
+
+    fun main() {
+
+        val bounds = gd.defaultConfiguration.bounds
+        println("bounds: $bounds")
+    }
+}
