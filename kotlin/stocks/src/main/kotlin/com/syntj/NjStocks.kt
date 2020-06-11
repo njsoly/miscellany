f213cb119c6179a6cb745399cd3c3191b9dce797
njsoly <njsoly@syntj.com>
2020-06-11 03:56:49 -0500

    stocks: NjStocks can now fetch quote info for Dropbox.

--- kotlin/stocks/src/main/kotlin/com/syntj/NjStocks.kt
+++ kotlin/stocks/src/main/kotlin/com/syntj/NjStocks.kt
@@ -1,11 +1,29 @@
 package com.syntj
 
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpClient.newHttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
 class NjStocks {
 
+    private val client: HttpClient = newHttpClient()
+
+    private val quoteUri = "https://finnhub.io/api/v1/quote?symbol=DBX&token=brgrd1frh5r9t6gjc440"
 
     fun run() {
         println("aoesunthaoesunth")
 
+        val request: HttpRequest = HttpRequest.newBuilder(
+            URI(quoteUri)
+        ).build()
+
+        val bodyHandler: HttpResponse.BodyHandler<String> = HttpResponse.BodyHandlers.ofString()
+
+        val response: HttpResponse<String> = client.send(request, bodyHandler)
+
+        println(response.body())
     }
 }
 
