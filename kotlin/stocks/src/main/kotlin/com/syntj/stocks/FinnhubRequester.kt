e9ad69e3390764cfc61a4db5bd8ba9c7b323a84a
njsoly <njsoly@syntj.com>
2020-06-13 03:42:24 -0500

    stocks: fitted new classes correctly, fixed build, organized and formatted

--- kotlin/stocks/src/main/kotlin/com/syntj/stocks/FinnhubRequester.kt
+++ kotlin/stocks/src/main/kotlin/com/syntj/stocks/FinnhubRequester.kt
@@ -1,10 +1,26 @@
 package com.syntj.stocks
 
 import java.net.URI
+import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.util.logging.Logger
 
 class FinnhubRequester {
+
+    companion object {
+        val API_KEY: String? = EnvGetter().getEnv("finnhub_api_key")
+
+        const val baseQuoteUri = "https://finnhub.io/api/v1/quote"
+    }
+
+    val logger = Logger.getLogger(javaClass.simpleName)
+
+    init {
+
+    }
+
+    private val client: HttpClient = HttpClient.newHttpClient()
 
     fun requestQuoteAsString(symbol: WatchedStock) : HttpResponse<String> {
         val request: HttpRequest = HttpRequest.newBuilder(
@@ -15,11 +31,10 @@ class FinnhubRequester {
 
         return client.send(request, bodyHandler)
     }
-
     private fun buildQuoteRequestUri(symbol: String): String {
         var uri = "$baseQuoteUri?symbol=$symbol"
-        if (NjStocks.API_KEY != null){
-            uri += "&token=${NjStocks.API_KEY}"
+        if (API_KEY != null){
+            uri += "&token=${API_KEY}"
         }
         logger.info("uri for quote $symbol: $uri")
         return uri
--- kotlin/stocks/src/main/kotlin/com/syntj/stocks/NjStocks.kt
+++ kotlin/stocks/src/main/kotlin/com/syntj/stocks/NjStocks.kt
@@ -1,37 +1,20 @@
 package com.syntj.stocks
 
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpClient.newHttpClient
-import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 import java.util.logging.Logger
 
 class NjStocks {
 
-    private val client: HttpClient = newHttpClient()
     private val logger = Logger.getLogger(javaClass.simpleName)
-
-    companion object {
-        @JvmStatic
-        val API_KEY: String? = EnvGetter().getEnv("finnhub_api_key")
-    }
-
-    private val baseQuoteUri = "https://finnhub.io/api/v1/quote"
-
 
     fun run() {
         println("************** NjStocks **************")
 
-        val quoteString: HttpResponse<String> = requestQuoteAsString(WatchedStock.DBX)
+        val quoteString: HttpResponse<String> = FinnhubRequester().requestQuoteAsString(WatchedStock.DBX)
 
         println(quoteString.body())
 
     }
-
-
-
-
 }
 
 
