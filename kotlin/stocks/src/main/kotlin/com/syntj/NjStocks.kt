e6c2f342188357d0619e19de788865b66165da85
njsoly <njsoly@syntj.com>
2020-06-13 02:46:18 -0500

    stocks: get my API key out of open code

--- kotlin/stocks/src/main/kotlin/com/syntj/NjStocks.kt
+++ kotlin/stocks/src/main/kotlin/com/syntj/NjStocks.kt
@@ -5,25 +5,43 @@ import java.net.http.HttpClient
 import java.net.http.HttpClient.newHttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.util.logging.Logger
 
 class NjStocks {
 
     private val client: HttpClient = newHttpClient()
+    private val logger = Logger.getLogger(javaClass.simpleName)
 
-    private val quoteUri = "https://finnhub.io/api/v1/quote?symbol=DBX&token=brgrd1frh5r9t6gjc440"
+    companion object {
+        @JvmStatic
+        val API_KEY: String? = System.getenv("finnhub_api_key")
+    }
+
+    private val baseQuoteUri = "https://finnhub.io/api/v1/quote"
+
+    private fun buildQuoteRequestUri(symbol: String): String {
+        var uri = "$baseQuoteUri?symbol=$symbol"
+        if (API_KEY != null){
+            uri += "&token=$API_KEY"
+        }
+        logger.info("uri for quote $symbol: $uri")
+        return uri
+    }
 
     fun run() {
-        println("aoesunthaoesunth")
+        println("************** NjStocks **************")
 
         val request: HttpRequest = HttpRequest.newBuilder(
-            URI(quoteUri)
+            URI(buildQuoteRequestUri("DBX"))
         ).build()
 
         val bodyHandler: HttpResponse.BodyHandler<String> = HttpResponse.BodyHandlers.ofString()
 
         val response: HttpResponse<String> = client.send(request, bodyHandler)
-
         println(response.body())
+
+//        println("System.getenv():${System.getenv()}")
+        println("System.getenv():${System.getenv("finnhub_api_key")}")
     }
 }
 
