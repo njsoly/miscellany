699e5cef0d292780a706bb4c1bd233cb421115a9
njsoly <njsoly@syntj.com>
2021-05-17 01:30:25 -0500

    progress on Euler156

--- kotlin/KotlinMaven/src/main/kotlin/Euler156.kt
+++ kotlin/KotlinMaven/src/main/kotlin/Euler156.kt
@@ -1,3 +1,9 @@
+import java.time.Clock
+import java.time.Instant
+import java.time.ZoneId
+import java.util.*
+import kotlin.collections.ArrayList
+
 /**
  * https://projecteuler.net/problem=156
  *
@@ -39,23 +45,108 @@
 class Euler156 {
 
     companion object {
-        const val MAX_N = 22786974071
+        const val MAX_N = 1111111110L
+//        var initMillis = Instant.now(Clock.systemUTC().withZone(ZoneId.of("America/Chicago"))).toEpochMilli()
+        var initMillis = System.currentTimeMillis()
+
+        fun millisSinceInit() : Long {
+            return System.currentTimeMillis() - initMillis
+        }
+
+        fun readableTimeFromMillis(millis: Long) : String {
+            var x = millis
+            val ms = millis % 1000
+            x /= 1000
+            val s = x % 60
+            x /= 60
+            val m = x
+
+            return if (m > 0) {
+                "${m}m ${s}.${ms}s"
+            } else {
+                "${s}.${ms}s"
+            }
+        }
+
+        fun readableTimeSinceInit() : String {
+            return readableTimeFromMillis(millisSinceInit())
+        }
+
     }
-    
-    val n = 1
 
-    val solutions: LinkedHashMap<Long, Long> = LinkedHashMap<Long, Long>()
 
-    fun findSolutionsofFofNandD (n: Long = 0, d: Long = 1, f: Long = 0) {
+    val solutions: HashMap<Int, LinkedList<Soln>> = HashMap<Int, LinkedList<Soln>>()
+    val s: ArrayList<Long> = ArrayList()
 
+    /** returns all the solutions of f(n,d) */
+    fun findSolutionsofFofNandD (d: Int = 1, maxN: Long = MAX_N) : List<Long> {
+        var lastSoln: Long? = 0L
+        solutions[d] = LinkedList<Soln>()
+
+        while (lastSoln != null && lastSoln < maxN) {
+            lastSoln = findNextSolutionOfF_N_D(lastSoln, d, maxN) ?: break
+
+            s[d] += lastSoln
+//            println(
+//                "solution: f(n,$d): n = $lastSoln. s($d) is now at ${s[d]}, after ${solutions[d]!!.size} solutions, ${
+//                    readableTimeFromMillis(
+//                        millisSinceInit()
+//                    )
+//                } into run"
+//            )
+
+            solutions[d]!!.add(Soln(lastSoln, d))
+
+            if (s[d] >= 22786974071L) {
+                break
+            }
+        }
+
+        println("done searching.")
+        return solutions[d]!!.map{it.n}
     }
 
-    class Soln (val n: Long, val d: Long)
+    /**
+     * [solution] - The last point (so far) where f(n,d)=n.  Serves as the starting point for f and n.
+     * returns null if maxN reached before next solution. */
+    private fun findNextSolutionOfF_N_D(solution: Long = 0L, d: Int = 1, maxN: Long = MAX_N) : Long? {
+
+        var f = solution
+
+        for (n in solution+1 .. maxN) {
+            f += n.toString().count { digit -> digit.toString().toInt() == d }
+
+            if (f == n) {
+                return f
+            }
+        }
+
+        return null
+    }
+
+    class Soln (val n: Long, val d: Int)
+
+    init {
+        for (i in 0..10) {
+            s.add(0)
+        }
+    }
 
 }
 
 fun main() {
     println("Euler #156.")
 
+    val euler = Euler156()
+    val timesElapsed = mutableMapOf<Int, String>()
+
+    for (d in 1..9) {
+        println("searching for solutions for f(n,$d).")
+        euler.findSolutionsofFofNandD(d, Euler156.MAX_N)
+        timesElapsed[d] = Euler156.readableTimeSinceInit()
+    }
+
+    println("finished.  total sum: ${euler.s.sum()}" +
+            "\ntimes: \n" + timesElapsed.values)
 
 }
