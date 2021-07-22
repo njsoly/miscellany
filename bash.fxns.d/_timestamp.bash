fb2dd5f398d6a635233ec5090f91f9e62952c548
njsoly <njsoly@syntj.com>
2021-07-21 23:39:01 -0500

    bash.fxns.d/_timestamp.bash: updates to date and time utility functions

--- bash.fxns.d/_timestamp.bash
+++ bash.fxns.d/_timestamp.bash
@@ -1,10 +1,20 @@
 #!/bin/bash
-_timestamp ()
+__time_with_milliseconds () {
+	_ts_time_full="$(date +%H:%M:%S.%N)";
+	_ts_time=${_ts_time_full:0:12}
+	echo $_ts_time
+}
+__seconds_since_epoch () {
+	date +%s
+}
+__date_yyyy-mm-dd () {
+	date +%F
+}
+__timestamp ()
 {
-	_ts_time="$(date +%H:%M:%S.%N)";
-	_ts_seconds=$(date +%s)
-	_ts_time=${_ts_time:0:12}
-	_ts_date="$(date +%F)"
-	_ts="$_ts_time $_ts_date"
-	echo "$_ts"
+	__timestamp_loggable
+}
+__timestamp_loggable ()
+{
+	echo "$(__date_yyyy-mm-dd) $(__time_with_milliseconds)"
 }
