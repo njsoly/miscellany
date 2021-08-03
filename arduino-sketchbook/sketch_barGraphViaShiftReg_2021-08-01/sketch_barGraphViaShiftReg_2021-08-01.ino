4b6cc90a7b219bf980f612d928ab3a98278ccb24
njsoly <njsoly@syntj.com>
2021-08-02 20:21:00 -0500

    sketch_barGraphViaShiftReg_2021-08-01: polish, clean-up

--- arduino-sketchbook/sketch_barGraphViaShiftReg_2021-08-01/sketch_barGraphViaShiftReg_2021-08-01.ino
+++ arduino-sketchbook/sketch_barGraphViaShiftReg_2021-08-01/sketch_barGraphViaShiftReg_2021-08-01.ino
@@ -9,29 +9,38 @@
  */
 
 #define DATA_LENGTH 10
+#define ROTR 0
+#define ROTL 1
+#define DEMO_MAX ROTL
 
 #include <Arduino.h>
+#include <avr/pgmspace.h>
 
 const boolean DEBUG = false;
 
 // pins
 // const int graphPins[DATA_LENGTH] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-const int RCLK = 2; // register storage clock
-const int SRCLK = 3; // shift register 
-const int SER = 4; // serial data output pin
-const int OUTPUT_DISABLE = 5; // OE_not
+#define RCLK 2 // register storage clock
+#define SRCLK 3 // shift register
+#define SER 4 // serial data output pin
+#define OUTPUT_DISABLE 5 // OE_not
 
 
-int graphData[DATA_LENGTH];
+uint8_t graphData[DATA_LENGTH];
 
-// last millis() count when the bar graph was pulsed / lit
-long flashLast = 0L;
-// how many millis to wait between flashing the bar graph
-long flashPeriod = 2L;
-// last millis() count when the demo data was stepped.
-long demoLast = 0L;
+// has the LED been flashed since the data has been changed
+boolean flashNeeded = true;
+
 // how many millis to wait between stepping the demo
-long demoPeriod = 100L;
+const long demoPeriod = 60L;
+// last millis() count when the demo data was stepped.
+unsigned long demoLast = 0L;
+
+// how often (in ms) to switch the demo style
+const long demoSwitchPeriod = 3000L;
+uint32_t demoSwitchLast = 0L;
+
+uint8_t demoMode = ROTR;
 
 void setup() {
 
@@ -48,14 +57,12 @@ void setup() {
   digitalWrite(OUTPUT_DISABLE, HIGH);
 
   writeBarGraph(0x010, graphData, DATA_LENGTH);
-  
-  flashLast = millis();
+  demoSwitchLast = millis();
 }
 
 void loop() {
-  if (flashLast + flashPeriod < millis()) {
+  if (flashNeeded) {
     flashBarGraph();
-    flashLast = millis();
   }
 
   if (demoLast + demoPeriod < millis()) {
@@ -63,13 +70,18 @@ void loop() {
     demoLast = millis();
   }
 
+  if (demoSwitchLast + demoSwitchPeriod < millis()) {
+    switchDemo();
+    demoSwitchLast = millis();
+  }
+
 }
 
 void shiftValue(int bit) {
+    digitalWrite(OUTPUT_DISABLE, HIGH);
     digitalWrite(SER, bit);
     digitalWrite(SRCLK, LOW);
     digitalWrite(RCLK, LOW);
-    digitalWrite(OUTPUT_DISABLE, HIGH);
     digitalWrite(SRCLK, HIGH);
     digitalWrite(RCLK, HIGH);
     digitalWrite(OUTPUT_DISABLE, LOW);
@@ -79,23 +91,49 @@ void flashBarGraph() {
     for (int i = DATA_LENGTH - 1; i >= 0; i--){
         shiftValue(graphData[i] > 0 ? HIGH : LOW);
     }
+    flashNeeded = false;
+}
 
-  delayMicroseconds(20);
+void flashBarGraphLow(int pinCount) {
+  for (int i = 0; i < pinCount; i++) {
+    shiftValue(LOW);
+  }
+
+  flashNeeded = false;
 }
 
 // write bit 0 of x to dataArray[0], bit 1 to dataArray[1], etc.
-void writeBarGraph(int x, int dataArray[], int dataArrayLength) {
+void writeBarGraph(int x, uint8_t dataArray[], int dataArrayLength) {
   for (int i = 0; i < dataArrayLength; i++){
     dataArray[i] = 0x1 & x;
     x = (unsigned int)x >> 1;
   }
+
+  flashNeeded = true;
+}
+
+void switchDemo() {
+  if (demoMode == DEMO_MAX) {
+    demoMode = 0;
+  } else {
+    demoMode++;
+  }
+  // make sure the first step of the next demo
+  // is triggered immediately.
+  demoLast = 0;
 }
 
 void stepDemo() {
-  graphDataRotateRight(graphData, DATA_LENGTH);
+  switch (demoMode) {
+    case ROTR: graphDataRotateRight(graphData, DATA_LENGTH); break;
+    case ROTL: graphDataRotateLeft(graphData, DATA_LENGTH); break;
+    default: break;
+  }
+
+  flashNeeded = true;
 }
 
-void graphDataRotateRight(int graphData[], int dataLength) {
+void graphDataRotateRight(uint8_t graphData[], int dataLength) {
   int temp = graphData[dataLength - 1];
   for (int i = dataLength - 1; i > 0; i--) {
     graphData[i] = graphData[i - 1];
@@ -104,8 +142,11 @@ void graphDataRotateRight(int graphData[], int dataLength) {
   graphData[0] = temp;
 }
 
-void allPinsLow(int pinCount) {
-  for (int i = 0; i < pinCount; i++) {
-    shiftValue(LOW);
+void graphDataRotateLeft(uint8_t graphData[], int dataLength) {
+  int temp = graphData[0];
+  for (int i = 0; i < dataLength - 1; i++) {
+    graphData[i] = graphData[i + 1];
   }
+
+  graphData[dataLength - 1] = temp;
 }
