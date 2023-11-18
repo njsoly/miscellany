899e65e6f640919876e4dc1e0267b927a15cf705
njsoly <njsoly@syntj.com>
2023-11-18 03:49:04 -0600

    setup_git.bash - rearranged

--- setup_git.bash
+++ setup_git.bash
@@ -7,10 +7,10 @@ git config --global user.email "njsoly@syntj.com"
 git config --global user.name "njsoly"
 git config --global alias.s "status"
 git config --global alias.co "checkout"
+git config --global alias.staged "diff --cached"
 git config --global pull.rebase false
 git config --global push.default upstream
 git config --global push.autosetupremote true
-git config --global alias.staged "diff --cached"
 git config --global core.filemode true
 git config --global core.ignorecase true
 git config --global core.logallrefupdates true
