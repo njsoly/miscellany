ab89b3d0e2c3e4092073386636e06386774a54bf
njsoly <njsoly@github.com>
2025-02-26 12:09:09 -0600

    git-generate-delete-statements-remote: fixed the actual git command

--- bash.fxns.d/git-generate-delete-statements-remote.bash
+++ bash.fxns.d/git-generate-delete-statements-remote.bash
@@ -1,6 +1,6 @@
 git-generate-delete-statements-remote () 
 { 
-    for b in $(git for-each-ref refs/remotes/origin --format "%(refname)" "echo %(refname)" | sed 's@refs/heads/@@g' );
+    for b in $(git for-each-ref refs/remotes/origin --format "%(refname)" "echo %(refname)" | sed 's@refs/remotes/@@g' );
     do
         echo "git branch -r -D $b";
     done
