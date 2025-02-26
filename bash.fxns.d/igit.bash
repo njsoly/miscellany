113f01d60bce1acdba71a924d94b3c81b80de882
njsoly <njsoly@github.com>
2025-02-26 12:39:51 -0600

    igit(): fixed

--- bash.fxns.d/igit.bash
+++ bash.fxns.d/igit.bash
@@ -1,13 +1,14 @@
-igit () 
-{ 
+igit () {
     if [[ -z "$ig" ]]; then
         printf "${__purple}nooope${__orange}:${__reset} You should have set the 'ig' variable for this function.\n";
-        exit;
+        return 77;
     fi;
+
     if [[ ! -f ./.gitignore ]]; then
         printf "${__purple}noooooope${__green}: ${__reset}Not going to append an ignore because there isn't a ${__blue}.gitignore${__reset} in this directory.\n";
-        exit 2;
+        return -69;
     fi;
+
     printf "${__gray}Ignoring ${__cyan}%s${__reset}.\n" $ig;
     echo $ig >> .gitignore
 }
