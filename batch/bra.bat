eac1a59612e45354e0c457e6b85ac4ee05e35f2e
njsoly <njsoly@syntj.com>
2026-03-18 03:02:55 -0500

    batch/bra.bat - added inline help

--- batch/bra.bat
+++ batch/bra.bat
@@ -1,2 +1,11 @@
-REM bra
+@echo off
+if "%1"=="-h" goto :help
+if "%1"=="--help" goto :help
 git branch -a %*
+goto :end
+
+:help
+echo bra.bat - CMD alias for 'git branch -a'
+echo     Passthrough for additional arguments.
+echo.
+:end
